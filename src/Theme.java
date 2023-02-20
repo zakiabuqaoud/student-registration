d38e6acfc2c4f7245648da03cf8d29a9a40229eb
zakiabuqaoud <20172106@student.alazhar.edu.ps>
2023-02-19 19:52:51 -0800

    delete files not important

---
+++ src/Theme.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+
+class Theme{
+
+    static Font bigFont = new Font("Cleaver's_Juvenia_Bloc...", Font.BOLD, 30);
+
+    static Font fontSmallBold = new Font("Cleaver's_Juvenia_Bloc...", Font.BOLD, 21);
+   static Font inputColor = new Font("Cleaver's_Juvenia_Bloc...", Font.BOLD, 15);
+    static Font smallFont = new Font("Cleaver's_Juvenia_Bloc...", Font.BOLD, 15);
+
+    static Font MediumFont = new Font("Cleaver's_Juvenia_Bloc...",Font.BOLD, 30);
+
+    static Color blackColor = Color.BLACK;
+    static Color whiteColor = Color.white;
+    static Color darkBlueColor = new Color(39,45,52);
+    static Color lightBrownColor = new Color(204,169,143);
+    static Color lightBlueColor = new Color(225,233,238);
+
+    static Color lightPurpleColor = new Color(229,204,255);
+    static Color darkPurpleColor = new Color(153,51,255);
+
+
+
+}
